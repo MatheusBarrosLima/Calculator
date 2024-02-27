4594e2e0139ef2f1a8ea691bf76960994ff21805
Matheus Barros Lima <matheusmatheusbarroslima6@gmail.com>
2024-02-27 16:15:28 -0300

    add locale storage expression

--- scripts/script.js
+++ scripts/script.js
@@ -4,9 +4,14 @@ const buttons = document.querySelectorAll(".gridButtons button");
 const buttonHistory= document.querySelector(".buttonHistory");
 const modalHistory= document.querySelector(".modalHistory");
 const buttonCloseModal = document.querySelector(".buttonCloseModal");
+const containerHistory = document.querySelector(".containerHistory");
+const buttonClearHistory = document.querySelector(".buttonClearHistory");
+
+
 
 buttonHistory.addEventListener("click", () => {
     modalHistory.showModal();
+    updateHistory();
 });
 
 buttonCloseModal.addEventListener("click", () => {
@@ -17,6 +22,25 @@ function clearScreen() {
     expression.innerHTML = "" 
     expressionDisplay.innerHTML = "" 
 };
+
+function updateHistory() {
+    const calculatorHistory = JSON.parse(localStorage.getItem("@calculator:history"));
+    
+    containerHistory.innerHTML = "";
+    if (calculatorHistory) {
+        calculatorHistory.forEach((value) => {
+        containerHistory.innerHTML += `
+            <div class="boxHistory">
+                <div class="expressionHistory">${value.expression}</div>
+                <div class="resultHistory">${value.result}</div>
+             </div>
+        `;    
+        })
+    } else {
+        console.log(calculatorHistory);
+        containerHistory.innerHTML = `<div class="emptyHistory">Vazio</div>`;
+    }
+}
 
 const totalNumbers = 13;
 let isError = false;
@@ -39,7 +63,29 @@ if (isError) {
                 if(expression.innerHTML.length > 0){
                     expressionDisplay.innerHTML = expression.innerHTML;
                 expression.innerHTML = String(
-                    eval(expression.innerHTML.replace("x", "*").replace("%", "/100"))).slice(0, 13);}
+                    eval(expression.innerHTML.replace("x", "*").replace("%", "/100"))).slice(0, totalNumbers);}
+
+                    const calculatorHistory = JSON.parse(localStorage.getItem("@calculator:history"));
+
+                    if(calculatorHistory) {
+                        localStorage.setItem("@calculator:history", JSON.stringify([
+                            ...calculatorHistory,
+                            {
+                            result: expression.innerHTML, 
+                            expression: expressionDisplay.innerHTML,
+                        },
+                               ])
+                        );
+                    } else {
+                        localStorage.setItem("@calculator:history", JSON.stringify([
+                            {
+                            result: expression.innerHTML, 
+                            expression: expressionDisplay.innerHTML,
+                        },
+                               ])
+                        );
+                    }
+                    
                 } catch (error) {
                 console.error(error);
                 expression.innerHTML = "Error";
