9aff4717203f29b469f9a68fa6d2b3408b427975
Matheus Barros Lima <matheusmatheusbarroslima6@gmail.com>
2024-02-28 15:11:40 -0300

    add logic clear history

--- scripts/script.js
+++ scripts/script.js
@@ -18,6 +18,19 @@ buttonCloseModal.addEventListener("click", () => {
     modalHistory.close();
 });
 
+buttonClearHistory.addEventListener("click", ()=> {
+    const calculatorHistory = JSON.parse(localStorage.getItem("@calculator:history"));
+
+    if (calculatorHistory) {
+        const responseToClear = confirm("Deseja limpar histórico?");
+    if (responseToClear) {
+        localStorage.removeItem("@calculator:history");
+        updateHistory();
+    }
+    }
+    
+})
+
 function clearScreen() {
     expression.innerHTML = "" 
     expressionDisplay.innerHTML = "" 
@@ -28,7 +41,7 @@ function updateHistory() {
     
     containerHistory.innerHTML = "";
     if (calculatorHistory) {
-        calculatorHistory.forEach((value) => {
+        calculatorHistory.reverse().forEach((value) => {
         containerHistory.innerHTML += `
             <div class="boxHistory">
                 <div class="expressionHistory">${value.expression}</div>
@@ -69,7 +82,7 @@ if (isError) {
 
                     if(calculatorHistory) {
                         localStorage.setItem("@calculator:history", JSON.stringify([
-                            ...calculatorHistory,
+                            ...calculatorHistory.slice(-20), // limit history
                             {
                             result: expression.innerHTML, 
                             expression: expressionDisplay.innerHTML,
